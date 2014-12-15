5e16cae4c72475c510f9645cb1ecaf1e77e614b1
CarolZamith <carolzc89@gmail.com>
2014-12-15 10:52:04 -0200

    Primeiro Commit

---
+++ print_teste.py
@@ -0,0 +1,20 @@
+__author__ = 'carolina_zamith'
+
+
+class printTeste:
+      #  def __init__(self,name):
+      #         self.name = name
+
+        def print_teste(a, b):
+            
+                """
+
+                :rtype : object
+                """
+                print (a, b)
+
+
+        
+
+
+
---
+++ view.py
@@ -0,0 +1,11 @@
+__author__ = 'carolina_zamith'
+
+from print_teste import printTeste
+
+
+class view:
+
+
+    x = printTeste()
+    x.print_teste(1)
+   # print x
